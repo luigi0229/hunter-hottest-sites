6ed9d53b01e1bfa1216d262309e1fbaa4a573305
Eli <luigi_229@msn.com>
2016-10-18 18:04:26 -0400

    fixed conflicts

--- src/packetAnalyzerFiltered.py
+++ src/packetAnalyzerFiltered.py
@@ -67,9 +67,6 @@ def examinePacket(pkt):
 
 
 def startCapture2():
-<<<<<<< HEAD
     capture = pyshark.LiveCapture(interface='en0s asdf', only_summaries=False)    #creates a new pyshark object with a specific interface and desired parameters 
-=======
     capture = pyshark.LiveCapture(interface='en0', only_summaries=False)    #creates a new pyshark object with a specific interface and desired parameters
->>>>>>> bb71a2585c1512d4242d2efaf2847b02745e8708
     capture.apply_on_packets(examinePacket)                                 #sends every packet to the examinePAcket function above to examine them
