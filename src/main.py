e3932e2ef3c73b9d487c96d57dccb8329838c592
Eli <luigi_229@msn.com>
2016-10-18 18:21:44 -0400

    fixed stuff

--- src/main.py
+++ src/main.py
@@ -12,19 +12,16 @@ def main():
     p = multiprocessing.Process(target=startCapture, name="startCapture") 
     p.start()
     time.sleep(1200)   #Time to run capture
-    #global trigger
-    p = multiprocessing.Process(target=startCapture, name="startCapture")
-    p2 = multiprocessing.Process(target=startCapture2, name="startCapture") 
-    p.start()
-    p2.start()
-    time.sleep(350)   #Time to run capture
+    
+    #p2 = multiprocessing.Process(target=startCapture2, name="startCapture") 
+    #p2.start()
+    #time.sleep(350)   #Time to run capture
 
-    #time.sleep(2)
 
     p.terminate()
     p.join()
-    p2.terminate()
-    p2.join()
+    #p2.terminate()
+    #p2.join()
     print "Packet pacture terminated"
 
 main()
