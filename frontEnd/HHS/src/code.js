ab97f757e8d99fccce3b896cdd56897b768e0e3a
Leon Qiu <Leon@Leons-MacBook-Air.local>
2016-10-23 19:35:50 -0400

    fixed some css

--- frontEnd/HHS/src/code.js
+++ frontEnd/HHS/src/code.js
@@ -8,14 +8,15 @@ d3.json("data.json", function(error,d){
 var width = window.innerWidth,
     height = 650;
 
-//var fill = d3.scale.category20b();
-
 var nodes = [], labels = [],
     foci = [{x: 650, y: 450}];
+
+var nodesByName = {};
 
 var svg = d3.select("body").append("svg")
     .attr("width", "100%")
     .attr("height", height)
+
 
 var force = d3.layout.force()
     .nodes(nodes)
@@ -44,12 +45,131 @@ function tick(e) {
 }
 
 
+var newData = [{  
+   "color":"green",
+   "name":"Slickdeals.net",
+   "r":100
+},
+{  
+   "color":"red",
+   "name":"Cloudfront.net",
+   "r":40
+},
+{  
+   "color":"blue",
+   "name":"Teamviewer.com",
+   "r":40
+},
+{  
+   "color":"#2dbfff",
+   "name":"Yahoo.com",
+   "r":30
+},
+{  
+   "color":"#7db1b2",
+   "name":"msn.com",
+   "r":40
+},
+{  
+   "color":"purple",
+   "name":"cuny.edu",
+   "r":45
+},
+{  
+   "color":"cyan",
+   "name":"apple.com",
+   "r":20
+},
+{  
+   "color":"#f2f205",
+   "name":"facebook.com",
+   "r":20
+},
+{  
+   "color":"orange",
+   "name":"github.com",
+   "r":20
+},
+{  
+   "color":"green",
+   "name":"spotify.com",
+   "r":20
+},
+{  
+   "color":"#800080",
+   "name":"purple.com",
+   "r":10
+},
+{  
+   "color":"red",
+   "name":"verizon.net",
+   "r":10
+},
+{  
+   "color":"red",
+   "name":"Netflix",
+   "r":10
+},
+{  
+   "color":"yellow",
+   "name":"globalcapacity.com",
+   "r":10
+},
+{  
+   "color":"yellow",
+   "name":"globalcapacity.com",
+   "r":50
+},
+{  
+   "color":"yellow",
+   "name":"globalcapacity.com",
+   "r":50
+},
+{  
+   "color":"yellow",
+   "name":"globalcapacity.com",
+   "r":50
+},
+{  
+   "color":"yellow",
+   "name":"globalcapacity.com",
+   "r":50
+}]
+
+
+setTimeout(function() { processUpdates(data) }, 3000);
+
+
+function processUpdates(sites) {
+  for (var i = 0; i < sites.length; i++) {
+    var site = sites[i];
+    if (nodesByName[site.name]) {
+      console.log(site.name, site.r)
+      updateNodeRadius(site.name, site.r)
+    } else {
+      // add new circle to graph
+    }
+  }
+}
+
+function updateNodeRadius(name, radius) {
+  var nodeObject = nodesByName[name];
+  console.log(nodeObject)
+  nodeObject[0][0].children[0].r.baseVal.value = radius;
+  force.start();
+}
+
+// setTimeout(function() { updateNodeRadius("Cloudfront.net", 500) }, 3000)
+
+
+
 var timer = setInterval(function(){
 
   if (nodes.length > data.length-1) { clearInterval(timer); return;}
 
   var item = data[counter];
-  nodes.push({color: item.color, r: item.r, name: item.name});
+  var nodeObject = {color: item.color, r: item.r, name: item.name};
+  nodes.push(nodeObject);
   force.start();
 
   node = node.data(nodes);
@@ -78,8 +198,11 @@ var timer = setInterval(function(){
        })
       .attr("dy", ".35em")
 
+    nodesByName[item.name] = n;
+
+
   counter++;
-}, 100);
+}, 0);
 
 
 d3.selection.prototype.moveToFront = function() {
